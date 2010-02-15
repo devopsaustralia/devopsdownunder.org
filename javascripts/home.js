d04c836e219fbf37ce262430fbd08cbb1595dfdc
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-02-14 21:46:17 -0500

    removed legacy code

--- javascripts/home.js
+++ javascripts/home.js
@@ -3,9 +3,5 @@ layout: nil
 ---
 window.addEvent('domready', function() { 
 
-    var doing = new Widget('doing', {url: '{{ site.url }}/data/doing.json', count: 3});
-    var seeing = new ImageWidget('seeing', {url: '{{ site.url }}/data/seeing.json', count: 5});
-    var listening = new ImageWidget('listening', {url: '{{ site.url }}/data/listening.json'});
-    
 });
 
